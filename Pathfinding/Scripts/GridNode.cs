7dec5ec1950a5fba47fefa1952887d50bf5b2ac6
Jasch0Jumper <ja.coding7@gmail.com>
2021-09-12 14:53:40 +0200

    added Pathfinding and General

---
+++ Pathfinding/Scripts/GridNode.cs
@@ -0,0 +1,20 @@
+﻿using CI.Tools;
+
+namespace CI.Pathfinding
+{
+	public class GridNode
+	{
+		public GridPosition Position { get; private set; }
+
+		public int Cost { get; private set; }
+
+		public bool Walkable { get; set; }
+
+		public GridNode(GridPosition gridPosition, int cost)
+		{
+			Position = gridPosition;
+			Cost = cost;
+			Walkable = true;
+		}
+	}
+}
