7e04b7519df6193bfda0b5f9825a917dd5d27f18
Jasch0Jumper <ja.coding7@gmail.com>
2021-12-10 11:25:49 +0100

    added own assembly definitions for editor folders of CI.Utilities and CI.SceneManagment

--- Utilities/Scripts/ExtensionMethods.cs
+++ Utilities/Scripts/ExtensionMethods.cs
@@ -12,7 +12,6 @@ namespace CI.Utilities
 			rounded.z = rounded.z.Round(places);
 			return rounded;
 		}
-
 		public static Vector2 Round(this Vector2 vector, int places = 3)
 		{
 			var rounded = vector;
@@ -20,12 +19,11 @@ namespace CI.Utilities
 			rounded.y = rounded.y.Round(places);
 			return rounded;
 		}
-
 		public static float Round(this float x, int places = 3)
 		{
-			float factor = 10f;
+			float factor = 1f;
 
-			for (int i = 0; i < places - 1; i++)
+			for (int i = 1; i <= places; i++)
 			{
 				factor *= 10f;
 			}
@@ -34,8 +32,6 @@ namespace CI.Utilities
 
 			return x;
 		}
-
-		public static Vector3 AsVector3(this Vector2 vector) => new Vector3(vector.x, vector.y, 0f);
 
 		public static Vector2 ToVector2(this Vector3 vector) => new Vector2(vector.x, vector.y);
 		public static Vector3 ToVector3(this Vector2 vector) => new Vector3(vector.x, vector.y, 0f);
@@ -50,9 +46,18 @@ namespace CI.Utilities
 			return component.transform.position + vector.ToVector3();
 		}
 
-		public static float RotateWith(this float angel, Component component)
+		public static Vector3 Rotate(this Vector3 vector, float angel)
 		{
-			return angel + component.transform.rotation.eulerAngles.z;
+			var q = Quaternion.Euler(0f, 0f, angel);
+
+			var vectorPartOfQ = new Vector3(q.x, q.y, q.z);
+			var scalerPartOfQ = q.w;
+
+			var newVector = 2f * Vector3.Dot(vectorPartOfQ, vector) * vectorPartOfQ
+							+ ((scalerPartOfQ * scalerPartOfQ) - Vector3.Dot(vectorPartOfQ, vectorPartOfQ)) * vector
+							+ 2f * scalerPartOfQ * Vector3.Cross(vectorPartOfQ, vector);
+
+			return newVector;
 		}
 	}
 }
